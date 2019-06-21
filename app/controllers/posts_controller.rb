7997afff41c73dfdc6667357f20a589ca2ab83d8
yamada <your.email@example.com>
2019-06-21 10:02:06 +0900

    dotinstall complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,13 +26,19 @@ class PostsController < ApplicationController
 
     end
 
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end
+
     private
     def post_params
         params.require(:post).permit(:content, :answer)
     end
 
-    def edit
-        @post = Post.find(params[:id])
-    end
+    # def edit
+    #     @post = Post.find(params[:id])
+    # end
 
 end
